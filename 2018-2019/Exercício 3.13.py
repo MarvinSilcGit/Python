fc84fe0123a6fb9e626e94bbfaa1d075f64f6550
marvin <marvinsilc@protonmail.ch>
2024-12-23 17:07:15 -0300

    Adding new codes missing before, fixing codes and organizing the structure for display on Markdown's Github

--- 2018-2019/Exercício 3.13.py
+++ 2018-2019/Exercício 3.13.py
@@ -1,10 +1,10 @@
-celsius = float(input("digite a temperatura para ser convertida de celsius para fahrenheit: "))
+celsius = float(input("Digite a temperatura para ser convertida de celsius para fahrenheit: "))
 
 fahrenheit = (celsius * 1.8) + 32
 
 print("A temperatura em %.1f° celsius equivale à %.1f° fahrenheit" % (celsius, fahrenheit))
 
-fahrenheit = float(input("digite a temperautra para ser convertida de fahrenheit para celsius: "))
+fahrenheit = float(input("Digite a temperautra para ser convertida de fahrenheit para celsius: "))
 
 celsius = (fahrenheit - 32) / 1.8
 
