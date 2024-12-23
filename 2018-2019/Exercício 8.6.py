5314f192d99b35b5af68139e28c6623903e2b411
marvin <marvinsilc@protonmail.ch>
2024-12-23 11:07:31 -0300

    Fixing minor erros

--- 2018-2019/Exercício 8.6.py
+++ 2018-2019/Exercício 8.6.py
@@ -1,39 +1,17 @@
-# def soma (lis):
+def soma(L):
 
-# total=0
+    total = 0
+    x = 0
 
-# x=0
+    while x<5:
 
-# for x, w in enumerate (lis):
+		total += L[x]
+		x += 1
 
-# total+=lis[x]
+    return total
 
-# return total,w
+L=[1,7,2,9,15]
 
-# lis=[1,7,2,9,15]
+print(soma(L))
 
-# print(soma(x))
-
-# print()
-
-# print(soma(lis))
-
-# print(soma([7,9,12,3,100,20,4]))
-
-z = 0
-
-a = 0
-
-while True:
-
-    print("%d:%d" % (a, z))
-
-    if z == 10:
-
-        a += 1
-
-        if a == 9:
-
-            z = 0
-
-    z += 1
+print(soma([7,9,12,3,100,20,4]))
