0e99600e41ef1931ac68b92abb849d12de44323b
marvin <marvinsilc@protonmail.ch>
2024-12-23 15:30:47 -0300

    Adding new codes missing before and organizing the structure

--- 2018-2019/Exercício 8.6.py
+++ 2018-2019/Exercício 8.6.py
@@ -2,17 +2,12 @@ def soma(lista):
 
     total = 0
 
-    contador = 0
+    for contador in lista:
 
-    while contador < len(lista):
-
-        total += lista[contador]
-
-        contador += 1
+        total += contador
 
     return total
 
 lista = [1, 7, 2, 9, 15]
 
 print(soma(lista))
-print(soma([7, 9, 12, 3, 100, 20, 4]))
