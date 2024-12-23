67b5f50e16cc670c7a2c1db4201c895089c6394f
marvin <marvinsilc@protonmail.ch>
2024-12-23 16:56:40 -0300

    Adding new codes missing before, fixing codes and organizing the structure for display on Markdown's Github

--- 2018-2019/Exercício 3.10.py
+++ 2018-2019/Exercício 3.10.py
@@ -2,8 +2,8 @@ salario = float(input("digite o salário recebido anualmente: "))
 
 porcentagem = float(input("qual a porcentagem de aumento? "))
 
-aumento = (porcentagem/10)*(salario/10)
+aumento = (porcentagem / 10) * (salario / 10)
 
-total = salario+aumento
+total = salario + aumento
 
 print("com um salario inicial de R$ %.2f e um aumento de R$ %.2f, o funcionario passará à receber R$ %.2f" % (salario, aumento, total))
--- 2018-2019/Exercício 3.11.py
+++ 2018-2019/Exercício 3.11.py
@@ -1,9 +1,9 @@
 precoMercadoria = float(input("Digite o preço da mercadoria: "))
 
-desconto = int(input("Digite a pocentagem de desconto"))
+desconto = float(input("Digite a pocentagem de desconto: "))
 
-valordesconto = (desconto/10)*(precoMercadoria/10)
+valordesconto = (desconto / 10 ) * (precoMercadoria / 10)
 
-precototal = precoMercadoria-desconto*10
+precototal = precoMercadoria - valordesconto
 
-print("O valor do desconto é de %.2f R$. E o preço à pagar é de %.2f R$" % (valordesconto, precototal))
+print("O valor do desconto é de R$%.2f. E o preço à pagar é de R$ %.2f" % (valordesconto, precototal))
