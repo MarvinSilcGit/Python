43dcaa84c64b6501d2140631790b73e1b35ee180
marvin <marvinsilc@protonmail.ch>
2024-12-23 15:37:55 -0300

    Adding new codes missing before and organizing the structure

---
+++ 2018-2019/Exercício 3.7.py
@@ -0,0 +1,5 @@
+numero1 = int(input("Digite o primeiro número: "))
+
+numero2 = int(input("Digite o segundo número: "))
+
+print(numero1 + numero2)
---
+++ 2018-2019/Exercício 3.8.py
@@ -0,0 +1,5 @@
+valorMetro = float(input("Digite o valor para ser convertido em milímetros: "))
+
+valorMilimetro = valorMetro * 1000
+
+print(valorMilimetro)
