b8225a990f4402df4712d72c889893c721ee6335
marvin <marvinsilc@protonmail.ch>
2024-12-24 14:22:59 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Decisão/6.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite o primeiro número: "))
+
+numero2 = float(input("Digite o segundo número: "))
+
+numero3 = float(input("Digite o terceiro número: "))
+
+if numero1 > numero2 > numero3:
+
+    print("O maior número é %.2f " % numero1)
