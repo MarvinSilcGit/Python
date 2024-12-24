7e135ac16c50d1de40de948abc770787ca21ec88
marvin <marvinsilc@protonmail.ch>
2024-12-24 14:46:49 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura Sequencial/8.py
@@ -0,0 +1,7 @@
+salarioHora = float(input("Digite seu salário por hora: "))
+
+horasMensal = float(input("Digite sua carga horária mensal: "))
+
+salario = salarioHora * horasMensal
+
+print("O salário mensal é R$ %.2f " % salario)
