30dfc957160b140f9e20e6f4094059ad94626612
marvin <marvinsilc@protonmail.ch>
2024-12-24 13:53:11 -0300

    Resolving new challenges

---
+++ Python Brasil Wiki/Estrutura de Decisão/1.py
@@ -0,0 +1,15 @@
+numero1 = int(input("Digite o primeiro número: "))
+
+numero2 = int(input("Digite o segundo número: "))
+
+if numero1 > numero2:
+
+    print("O maior número é %d" % numero1)
+
+elif numero1 < numero2:
+
+    print("O maior número é %d" % numero2)
+
+else:
+
+    print("Os dois números são iguais")
---
+++ Python Brasil Wiki/Estrutura de Decisão/2.py
@@ -0,0 +1,9 @@
+valor = float(input("Digite um número: "))
+
+if valor < 0:
+
+    print("O valor %.1f é negativo" % valor)
+
+else:
+
+    print("O valor %.1f é positivo" % valor)
