a08870ba96c16b03360e39d093477c27c79a2881
marvin <marvinsilc@protonmail.ch>
2024-12-24 17:29:53 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura Sequencial/13.py
@@ -0,0 +1,11 @@
+alturaHomem = float(input("Digite a altura do homem: "))
+
+alturaMulher = float(input("Digite a altura da mulher: "))
+
+pesoIdealHomem = (72.7 * alturaHomem) - 58
+
+pesoIdealMulher = (62.1  * alturaMulher) - 44.7
+
+print("O peso ideal desse homem é %.2f" % pesoIdealHomem)
+
+print("O peso ideal dessa mulher é %.2f" % pesoIdealMulher)
