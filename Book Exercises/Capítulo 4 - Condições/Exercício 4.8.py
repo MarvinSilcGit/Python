a958f427280ea65588b46ee5868738eaeb910b06
marvin <marvinsilc@protonmail.ch>
2024-12-25 10:51:07 -0300

    fixing minor erros

--- Book Exercises/Capítulo 4 - Condições/Exercício 4.8.py
+++ Book Exercises/Capítulo 4 - Condições/Exercício 4.8.py
@@ -8,33 +8,39 @@ print("Digite 3 para multiplicar")
 
 print("Digite 4 para dividir")
 
-operadores = int(input("Qual operador deseja utilizar na operação? "))
+print()
+
+operadores = input("Qual operador deseja utilizar na operação? ")
+
+print()
 
 numero2 = float(input("Digite o segundo valor: "))
 
-if operadores == 1:
+print()
+
+if operadores == "1":
 
     soma = numero1 + numero2
 
-    print("O resultado da soma entre %.2f e %.2f é %.2f" % (numero1, numero2, soma))
+    print("O resultado da soma entre %.1f e %.1f é %.1f" % (numero1, numero2, soma))
 
-elif operadores == 2:
+elif operadores == "2":
 
     soma = numero1 - numero2
 
-    print("O resultado da subtração entre %.2f e %.2f é %.2f" % (numero1, numero2, soma))
+    print("O resultado da subtração entre %.1f e %.1f é %.1f" % (numero1, numero2, soma))
 
-elif operadores == 3:
+elif operadores == "3":
 
     soma = numero1 * numero2
 
-    print("O resultado da multiplicação entre %.f e %.2f é %.2f" % (numero1, numero2, soma))
+    print("O resultado da multiplicação entre %.1f e %.1f é %.1f" % (numero1, numero2, soma))
 
-elif operadores == 4:
+elif operadores == "4":
 
     soma = numero1 / numero2
 
-    print("O resultado da divisão entre %.2f e %.2f é %.2f" % (numero1, numero2, soma))
+    print("O resultado da divisão entre %.1f e %.1f é %.1f" % (numero1, numero2, soma))
 
 elif operadores != 1 and operadores != 2 and operadores != 3 and operadores != 4:
 
