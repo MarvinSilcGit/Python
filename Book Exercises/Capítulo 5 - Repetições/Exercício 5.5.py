b0447b16121353f04eabf7d982d16da13c764d42
marvin <marvinsilc@protonmail.ch>
2024-12-25 16:34:55 -0300

    fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.5.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.5.py
@@ -1,21 +1,3 @@
-cont = 1
+for contador in range(11):
 
-while cont != 0:
-
-    valor1 = int(input("digite um valor :"))
-
-    valor2 = valor1+valor1
-
-    while valor2 <= valor1*11:
-
-        print(valor2)
-
-        valor2 = valor2+valor1
-
-    print()
-
-    cont = int(input("Digite 0 para interromper a execução: "))
-
-    if cont == 0:
-
-        break
+    print(contador * 3)
