07f67549f0e1fdcd393f11d523cd133336481aed
marvin <marvinsilc@protonmail.ch>
2024-12-26 14:42:33 -0300

    fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.11-12.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.11-12.py
@@ -1,49 +1,49 @@
-cont = 1
+contador1 = 1
 
-while cont != 0:
+while contador1 != 0:
     
-    deposito = float(input("Qual o valor do depósito inicial na conta? "))
+    depositoInicial = float(input("Qual o valor do depósito inicial na conta? "))
 
-    mensal = float(input("Qual o valor depositado mensalmente? "))
+    depositoMensal = float(input("Qual o valor depositado mensalmente? "))
 
-    taxa = float(input("Qual a taxa de juros da poupança no período? "))
+    taxaJuros = float(input("Qual a taxa de juros da poupança no período? "))
 
-    cont = 1
+    contador1 = 1
 
     ganho = 0
 
-    while cont <= 24:
+    for contador2 in range(24):
 
-        ganho = (deposito)+(deposito/100)*taxa
+        ganho = depositoInicial + (depositoInicial / 100) * taxaJuros
 
-        if ganho > deposito:
+        if ganho > depositoInicial:
 
-            print("O valor do depósito no %d° mês era de %.2f, com uma taxa de %.2f por cento, resultando no valor de %.2f R$ reais " % (cont, deposito, taxa, ganho))
+            print("O valor do depósito no %d° mês era de %.2f, com uma taxa de %.2f por cento, resultando no valor de %.2f R$ reais " % (contador1, depositoInicial, taxaJuros, ganho))
 
-            deposito = ganho
+            depositoInicial = ganho
 
-            cont = cont+1
+            contador1 += 1
 
-            deposito = deposito+mensal
+            depositoInicial = depositoInicial + depositoMensal
 
-        elif ganho == deposito:
+        elif ganho == depositoInicial:
 
-            print("No mês %d não teve aumento em relação ao mês anterior na poupança" % cont)
+            print("No mês %d não teve aumento em relação ao mês anterior na poupança" % contador1)
 
-            deposito = ganho
+            depositoInicial = ganho
 
-            cont = cont+1
+            contador1 += 1
 
-        elif ganho < deposito:
+        elif ganho < depositoInicial:
 
-            print("No mês %d teve uma redução de %.2f para %2.f, em relação ao mês passado" % (cont, deposito, ganho))
+            print("No mês %d teve uma redução de %.2f para %2.f, em relação ao mês passado" % (contador1, depositoInicial, ganho))
 
-            deposito = ganho
+            depositoInicial = ganho
 
-            cont = cont+1
+            contador1 += 1
 
     print()
 
-    cont = int(input("Digite 0 para interromper a execução: "))
+    contador1 = int(input("Digite 0 para interromper a execução: "))
 
     print()
