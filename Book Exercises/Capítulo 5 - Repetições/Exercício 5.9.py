6ffec97d1b3656322fed1c77271556a99ce19b3a
marvin <marvinsilc@protonmail.ch>
2024-12-25 21:45:23 -0300

    fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.9.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.9.py
@@ -16,21 +16,43 @@ while contador1 != 0:
 
     while True:
 
-        restoInteiro += valor2
+        if valor1 > valor2:
 
-        contador2 += 1
+            restoInteiro += valor2
 
-        if restoInteiro + valor2 == valor1:
+            contador2 += 1
 
-            restoInteiro = contador2 + 1
+            if restoInteiro + valor2 == valor1:
 
-            contador2 = 0
+                restoInteiro = contador2 + 1
+
+                contador2 = 0
+
+                print("O resto inteiro da divisão entre %d e %d é %d e o resto da divisão é: %d" % (valor1, valor2, restoInteiro, resto))
+
+                break
+
+        elif valor1 == valor2:
+
+            restoInteiro = 1
+
+            resto = 0
+
+            print("O resto inteiro da divisão entre %d e %d é %d e o resto da divisão é: %d" % (valor1, valor2, restoInteiro, resto))
 
             break
 
-    print("O resto inteiro da divisão entre %d e %d é %d e o resto da divisão é: %d" % (valor1, valor2, restoInteiro, resto))
+        else:
+
+            restoInteiro = 0
+
+
 
     print()
+
+    restoInteiro = 0
+
+    resto = 0
 
     contador1 = int(input("Digite 0 para interromper a execução: "))
 
