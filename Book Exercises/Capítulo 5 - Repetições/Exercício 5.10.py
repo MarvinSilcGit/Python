4caa888ebf211cdff5c9734a1a9aca1809df05d0
marvin <marvinsilc@protonmail.ch>
2024-12-26 14:34:36 -0300

    fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.10.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.10.py
@@ -8,24 +8,26 @@ while contador != 0:
 
     while questao <= 3:
 
-        resposta = input("reposta da questao %d : " % questao)
+        resposta = input("Digite a reposta da questao %d : " % questao)
 
         if questao == 1 and resposta == "b" or resposta == "B":
 
-            pontos = pontos+1
+            pontos = pontos + 1
 
         if questao == 2 and resposta == "a" or resposta == "A":
 
-            pontos = pontos+1
+            pontos = pontos + 1
 
         if questao == 3 and resposta == "d" or resposta == "D":
 
-            pontos = pontos+1
+            pontos = pontos + 1
 
         questao += 1
 
-        print(" O aluno fez %d pontos(s)  " % pontos)
+        print(" O aluno fez %d pontos(s) " % pontos)
 
     print()
 
     contador = int(input("Digite 0 para interromper a execução: "))
+
+    print()
