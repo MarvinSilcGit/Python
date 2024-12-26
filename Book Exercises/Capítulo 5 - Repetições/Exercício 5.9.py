356f1fcc487b0238961dbb8d09416aa68bac00fa
marvin <marvinsilc@protonmail.ch>
2024-12-26 12:26:46 -0300

    fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.9.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.9.py
@@ -1,10 +1,10 @@
 contador1 = 1
 
-restoInteiro = 0
-
-resto = 0
-
 while contador1 != 0:
+
+    restoInteiro = 0
+
+    resto = 0
 
     valor1 = float(input("Digite o primeiro valor: "))
 
@@ -12,37 +12,19 @@ while contador1 != 0:
 
     if valor1 == valor2:
 
-        restoInteiro = (valor1 - valor2) + 1
-
-        resto = valor1 - valor2
+        restoInteiro = 1
 
         print()
 
-        print("O resto inteiro da divisão entre %d e %d é: %d. E o resto da divisão é: %d" % (valor1, valor2, restoInteiro, resto))
-
-        restoInteiro = 0
-
     elif valor1 > valor2:
 
-        resto = 0
-
-        while valor1 > resto:
+        while valor2 + resto < valor1:
 
             restoInteiro += 1
 
             resto += valor2
 
-            if valor2 + resto > valor1:
-
-                break
-
         resto = valor1 - resto
-
-        print("O resto inteiro da divisão entre %d e %d é: %d. E o resto da divisão é: %d" % (valor1, valor2, restoInteiro, resto))
-
-        restoInteiro = 0
-
-        resto = 0
 
     else:
 
@@ -58,15 +40,7 @@ while contador1 != 0:
 
             resto += 1
 
-            if resto == valor2:
-
-                break
-
-        restoInteiro = 0
-
-        print("O resto inteiro da divisão entre %d e %d é: %d. E o resto da divisão é: %d" % (valor1, valor2, restoInteiro, resto))
-
-        resto = 0
+    print("O resto inteiro da divisão entre %d e %d é: %d. E o resto da divisão é: %d" % (valor1, valor2, restoInteiro, resto))
 
     print()
 
