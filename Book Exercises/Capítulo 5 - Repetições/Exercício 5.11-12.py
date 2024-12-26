26da6f4f7a28d7cfbf99635a2044971328fe021a
marvin <marvinsilc@protonmail.ch>
2024-12-26 15:49:12 -0300

    fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.11-12.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.11-12.py
@@ -1,46 +1,32 @@
 contador1 = 1
 
 while contador1 != 0:
-    
+
     depositoInicial = float(input("Qual o valor do depósito inicial na conta? "))
 
     depositoMensal = float(input("Qual o valor depositado mensalmente? "))
 
-    taxaJuros = float(input("Qual a taxa de juros da poupança no período? "))
+    taxaJuros = float(input("Qual a taxa de juros anual da poupança no período? "))
 
-    ganhoTotal = 0
-
-    for contador2 in range(1, 25):
-
-        ganhoTotal = depositoInicial + (depositoInicial / 100) * taxaJuros
-
-        if ganhoTotal > depositoInicial:
-
-            print("O valor do depósito no %d° mês era de %.2f. Com uma taxa de %.2f por cento, gerou o valor de R$ %.2f " % (contador2, depositoInicial, taxaJuros, ganhoTotal))
-
-            depositoInicial = ganhoTotal
-
-            contador1 += 1
-
-            depositoInicial = depositoInicial + depositoMensal
-
-        elif ganhoTotal == depositoInicial:
-
-            print("No mês %d não teve aumento em relação ao mês anterior na poupança" % contador2)
-
-            depositoInicial = ganhoTotal
-
-            contador1 += 1
-
-        elif ganhoTotal < depositoInicial:
-
-            print("No mês %d teve uma redução de %.2f para %2.f, em relação ao mês passado" % (contador2, depositoInicial, ganhoTotal))
-
-            depositoInicial = ganhoTotal
-
-            contador1 += 1
+    periodo = int(input("Digite a quantidade de meses do rendimento: "))
 
     print()
+
+    ganhoTotal = depositoInicial
+
+    ganhoJuros = depositoMensal + (ganhoTotal / 100 * taxaJuros / 12)
+
+    for contador2 in range(1, periodo + 1):
+
+        ganhoTotal += ganhoJuros
+
+        print("O valor na conta no %d° mês era de %.2f. Com uma taxa de %.2f por cento, gerou o valor de R$ %.2f " % (contador2, ganhoTotal, taxaJuros, ganhoJuros))
+
+        ganhoJuros += depositoMensal + (ganhoTotal / 100 * taxaJuros / 12)
+
+    print()
+
+    ganhoTotal = 0
 
     contador1 = int(input("Digite 0 para interromper a execução: "))
 
