59cac0f8b0da0e3630b30fab4994e821bca3a181
marvin <marvinsilc@protonmail.ch>
2024-12-27 21:05:14 -0300

    Fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.23-24.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.23-24.py
@@ -6,24 +6,26 @@ while True:
 
     numeroPrimo = int(input("Digite um número inteiro para saber se ele é primo ou não: "))
 
-    if numeroPrimo % 2 == 0 and numeroPrimo != 2:
+    if numeroPrimo == 0 or numeroPrimo == 1:
 
-        print()
-
-        print("O número %d não é primo" % numeroPrimo)
+        print("Esse número é inválido")
 
         continue
 
-    if numeroPrimo == 2:
-
-        contador = contador + 1
-
     else:
 
-        primos = (numeroPrimo % 2)
+        if numeroPrimo % 2 == 0 and numeroPrimo != 2:
 
-        contador += 1
+            print()
 
-    print()
+            print("%d não é primo" % numeroPrimo)
 
-    print("%d é um número primo" % numeroPrimo)
+            continue
+
+        if numeroPrimo == 2:
+
+            contador += 1
+
+        print()
+
+        print("%d é um número primo" % numeroPrimo)
