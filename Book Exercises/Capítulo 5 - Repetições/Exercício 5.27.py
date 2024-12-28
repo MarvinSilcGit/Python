89e805e353fc645bf37a96ca99066043c6529ed9
marvin <marvinsilc@protonmail.ch>
2024-12-27 21:00:27 -0300

    Fixing minor erros

--- Book Exercises/Capítulo 5 - Repetições/Exercício 5.27.py
+++ Book Exercises/Capítulo 5 - Repetições/Exercício 5.27.py
@@ -1,34 +1,34 @@
 contador1 = 1
 
-contador3 = 0
-
 while contador1 != 0:
+
+    contador3 = 0
 
     valor1 = (input("Digite um número ou uma letra para saber se é palíndromo: "))
 
-    for contador2 in range(0, len(valor1)):
+    if len(valor1) < 2:
 
-        posicao1 = (valor1 [0 + contador2])
+        print("Tamanho insuficiente!")
 
-        contador2 += 1
-
-        posicao2 = (valor1 [0 - contador2])
-
-        if posicao1 == posicao2:
-
-            contador3 += 1
-
-    if contador3 == contador2:
-
-        print("%s é palíndromo" % valor1)
+        continue
 
     else:
 
-        print("%s não é palíndromo" % valor1)
+        for contador2 in range(0, len(valor1)):
 
-    print()
+            if valor1 [0 + contador2] == valor1 [-1 - contador2]:
 
-    contador3 = 0
+                contador3 += 1
+
+        if contador3 == len(valor1):
+
+            print("%s é palíndromo" % valor1)
+
+        else:
+
+            print("%s não é palíndromo" % valor1)
+
+        print()
 
     contador1 = int(input("Digite 0 para interromper a execução: "))
 
