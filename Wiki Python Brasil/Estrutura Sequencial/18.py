2c438f30a73179f0d445351ddf71a1f81ac166bc
marvin <marvinsilc@protonmail.ch>
2024-12-30 16:51:23 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura Sequencial/18.py
@@ -0,0 +1,16 @@
+contador = 1
+
+while contador != 0:
+
+    tamanhoArquivo = float(input("Digite o tamanho do arquivo em Megabytes: "))
+
+    velecidadeLink = float(input("Digite a velocidade da conexão em megabits: "))
+
+
+
+
+    print()
+
+    contador =int(input("Digite 0 para interromper a execução: "))
+
+    print()
