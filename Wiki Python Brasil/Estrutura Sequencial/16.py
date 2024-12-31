57b0f175ebad56dcd2096d33b5d3f6991dd904a7
marvin <marvinsilc@protonmail.ch>
2024-12-30 22:01:10 -0300

    Resolving new challenges

--- Wiki Python Brasil/Estrutura Sequencial/16.py
+++ Wiki Python Brasil/Estrutura Sequencial/16.py
@@ -1,16 +1,20 @@
 metragem = float(input("Digite a metragem: "))
 
-metragemLata = 18
+coberturaTinta = 3
+
+metragemLata = 18 * coberturaTinta
 
 quantidadeLatas = 0
 
 precoFinal = 0
 
+precoLata = 80
+
 if metragem / metragemLata <= 1:
 
     quantidadeLatas = 1
 
-    precoFinal = quantidadeLatas * 80
+    precoFinal = quantidadeLatas * precoLata
 
     print("Será necessária, no máximo, uma lata de tinta para pintar %.2f metros². O custo será R$ %.2f" % (metragem, precoFinal))
 
@@ -20,14 +24,16 @@ else:
 
         quantidadeLatas = metragem / metragemLata
 
-        precoFinal = quantidadeLatas * 80
+        precoFinal = quantidadeLatas * precoLata
 
         print("Será necessária exatas %d latas de tinta para pintar %.2f metros². O custo será R$ %.2f" % (quantidadeLatas, metragem, precoFinal))
 
     else:
 
-        quantidadeLatas = metragem / metragemLata + 1
+        quantidadeLatas = (metragem // metragemLata) + 1
 
-        precoFinal = quantidadeLatas * 80
+        precoFinal = quantidadeLatas * precoLata
 
-        print("Será necessária aos menos %d latas de tinta para pintar %.2f metros². O custo será R$ %.2f" % (quantidadeLatas, metragem, precoFinal))
+        print(quantidadeLatas)
+
+        print("Será necessária aos menos %d latas de tinta para pintar %.1f metros². O custo será R$ %.2f" % (quantidadeLatas, metragem, precoFinal))
