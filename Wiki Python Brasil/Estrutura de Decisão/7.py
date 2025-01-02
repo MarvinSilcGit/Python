42318274dac8b45d6d3a2c03afaa7caad69f9e98
marvin <marvinsilc@protonmail.ch>
2025-01-01 21:40:21 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Decisão/7.py
@@ -0,0 +1,17 @@
+numero1 = float(input("Digite o primeiro número: "))
+
+numero2 = float(input("Digite o segundo número: "))
+
+numero3 = float(input("Digite o terceiro número: "))
+
+if numero1 > numero2 and numero1 > numero3:
+
+    print("O primeiro número é maior")
+
+elif numero2 > numero1 and numero2 > numero3:
+
+    print("O segundo número é maior")
+
+elif numero3 > numero2  and numero3 > numero1:
+
+    print("O terceiro número é maior")
