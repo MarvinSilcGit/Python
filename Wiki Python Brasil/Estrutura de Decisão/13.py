d83b800c25f22435bbf5f8855b90e1d4a32b60eb
marvin <marvinsilc@protonmail.ch>
2025-01-01 22:46:31 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Decisão/13.py
@@ -0,0 +1,33 @@
+diaSemana = input("Digite o dia da semana:\n1 - Domingo\n2 - Segunda-feira\n3 - Terça-feira\n4 - Quarta-feira\n5 - Quinta-feira\n6 - Sexta-feira\n7 - Sábado\n")
+
+if diaSemana == "1":
+
+    print("Domingo")
+
+elif diaSemana == "2":
+
+    print("Segunda-feira")
+
+elif diaSemana == "3":
+
+    print("Terça-feira")
+
+elif diaSemana == "4":
+
+    print("Quarta-feira")
+
+elif diaSemana == "5":
+
+    print("Quinta-feira")
+
+elif diaSemana == "6":
+
+    print("Sexta-feira")
+
+elif diaSemana == "7":
+
+    print("Sábado")
+
+else:
+
+    print("Valor inválido")
