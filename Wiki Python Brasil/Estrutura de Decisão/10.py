979b7ab0435f6fbd3b5fcc26cad449f84e37f249
marvin <marvinsilc@protonmail.ch>
2025-01-01 22:16:14 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Decisão/10.py
@@ -0,0 +1,17 @@
+turno = input("Digite o turno:\nN - Noturo\nM - Matutino\nV - Vespertino\n")
+
+if turno == "N" or turno == "n":
+
+    print("Boa noite")
+
+elif turno == "M" or turno == "m":
+
+    print("Bom dia")
+
+elif turno == "V" or turno == "v":
+
+    print("Boa tarde")
+
+else:
+
+    print("Valor inválido")
