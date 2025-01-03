470b5484fdfd87876322cd014d44ab5750bcd8bb
marvin <marvinsilc@protonmail.ch>
2025-01-03 16:00:14 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Repetição/14.py
@@ -0,0 +1,11 @@
+for contador in range(10):
+
+    numero = int(input("Digite o número para saber se é par ou ímpar: "))
+
+    if numero % 2 != 0:
+
+        print("O número %d é par" % numero)
+
+    else:
+
+        print("O número %d é ímpar" % numero)
