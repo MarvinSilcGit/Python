3c0948045514f4bbb0f18a93b505436a14fbe85e
marvin <marvinsilc@protonmail.ch>
2025-01-02 22:12:40 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Decisão/27.py
@@ -0,0 +1,23 @@
+quantidadeMorango = int(input("Digite a quantidade em kilos de morangos: "))
+
+quantidadeMaca = int(input("Digite a quantidade em kilos de maçãs: "))
+
+precoFinal = 0
+
+if quantidadeMorango <= 5:
+
+    precoFinal += quantidadeMorango * 2.5
+
+else:
+
+    precoFinal += quantidadeMorango * 2.2
+
+if quantidadeMaca <= 5:
+
+    precoFinal += quantidadeMaca * 1.8
+
+else:
+
+    precoFinal += quantidadeMaca * 1.2
+
+print("O preço final será R$ %.2f" % precoFinal)
