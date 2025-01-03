bf3ecdcaa1862f2ece09ab7f0c857bf28da412d7
marvin <marvinsilc@protonmail.ch>
2025-01-03 14:30:04 -0300

    Resolving new challenges

--- Wiki Python Brasil/Estrutura de Repetição/17.py
+++ Wiki Python Brasil/Estrutura de Repetição/17.py
@@ -1,11 +1,9 @@
-def calculo_fatorial (numero: int):
+numero = int(input("Digite um número para saber a fatorial: "))
 
-    numero = numero
+resultado_fatorial = numero
 
-    resultado_fatorial = numero
+for contador in range(numero, 1, -1):
 
-    for contador in range(numero, 1, -1):
+    resultado_fatorial *= contador - 1
 
-        resultado_fatorial *= contador - 1
-
-    return "Fatorial de %d é %d" % (numero, resultado_fatorial)
+print("Fatorial de %d é %d" % (numero, resultado_fatorial))
