80d7c4bc04977992befcb6d30f73a8b32b06465f
marvin <marvinsilc@protonmail.ch>
2025-01-03 11:47:26 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Repetição/4.py
@@ -0,0 +1,19 @@
+araja = 80000
+
+bacalaca = 200000
+
+anos = 0
+
+while araja < bacalaca:
+
+    araja += araja / 100 * 3
+
+    bacalaca += bacalaca / 100 * 1.5
+
+    anos +=1
+
+    print(araja)
+
+    print(bacalaca)
+
+print("Serão necessários pelos menos %d anos" % anos)
