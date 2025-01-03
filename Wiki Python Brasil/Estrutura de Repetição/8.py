5a742de0bcb24f5e1cebb01a16cf9f9d586c1f09
marvin <marvinsilc@protonmail.ch>
2025-01-03 15:48:46 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Repetição/8.py
@@ -0,0 +1,15 @@
+for contador in range(1) :
+
+    numero1 = float(input("Digite o primeiro número "))
+
+    numero2 = float(input("Digite o segundo número: "))
+
+    numero3 = float(input("Digite o terceiro número: "))
+
+    numero4 = float(input("Digite o quarto número: "))
+
+    numero5 = float(input("Digite o quinto número: "))
+
+    media = (numero1 + numero2 + numero3 + numero4 + numero5) / 5
+
+    print("O soma foi %.2f e a média foi %.2f" % ((numero1 + numero2 + numero3 + numero4 + numero5), media))
