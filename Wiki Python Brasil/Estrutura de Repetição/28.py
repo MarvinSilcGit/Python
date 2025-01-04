b3bf5f6ad4da177c3380bec8163a2c85424b795b
marvin <marvinsilc@protonmail.ch>
2025-01-04 16:32:58 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Repetição/28.py
@@ -0,0 +1,23 @@
+contador = 1
+
+custoTotal = 0
+
+while contador != 0:
+
+    quantidadeCds = int(input("Digite a quantidade de CDs na coleção: "))
+
+
+
+    for contador2 in range(1, quantidadeCds + 1):
+
+        print("Digite o valor do cd %d" % contador2)
+
+        valorCd = float(input(""))
+
+
+
+    print()
+
+    contador = int(input("Digite 0 para interromper a execução: "))
+
+    print()
