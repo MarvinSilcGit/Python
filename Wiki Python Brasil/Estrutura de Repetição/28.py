40802b9e7ee4ada283abb67e84022ea64b4e86a2
marvin <marvinsilc@protonmail.ch>
2025-01-04 16:37:43 -0300

    Resolving new challenges

--- Wiki Python Brasil/Estrutura de Repetição/28.py
+++ Wiki Python Brasil/Estrutura de Repetição/28.py
@@ -2,19 +2,23 @@ contador = 1
 
 custoTotal = 0
 
+custoMedio = 0
+
 while contador != 0:
 
     quantidadeCds = int(input("Digite a quantidade de CDs na coleção: "))
 
-
-
     for contador2 in range(1, quantidadeCds + 1):
 
-        print("Digite o valor do cd %d" % contador2)
+        print("Digite o valor do %d° CD" % contador2)
 
         valorCd = float(input(""))
 
+        custoTotal += valorCd
 
+    custoMedio = custoTotal / quantidadeCds
+
+    print("O custo total foi %.2f, e o custo médio de cada CD é R$ %.2f" % (custoTotal, custoMedio))
 
     print()
 
