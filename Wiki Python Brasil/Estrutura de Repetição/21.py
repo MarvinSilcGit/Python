08ad1df3f3e2a1356a5f472ee6902e6a9550be7f
marvin <marvinsilc@protonmail.ch>
2025-01-04 10:29:09 -0300

    Resolving new challenges

--- Wiki Python Brasil/Estrutura de Repetição/21.py
+++ Wiki Python Brasil/Estrutura de Repetição/21.py
@@ -1,41 +1,29 @@
-contador = 1
+confirmacao = 2
 
-while contador != 0:
+numero = int(input("Digite um número inteiro para saber se ele é primo ou não: "))
 
-    confirmacao = 2
+contador = 5
 
-    numero = int(input("Digite um número inteiro para saber se ele é primo ou não: "))
+if numero == 0 or numero == 1:
 
-    contador2 = 5
+    print("Esse número é inválido")
 
-    if numero == 0 or numero == 1:
+if numero == 2 or numero == 3:
 
-        print("Esse número é inválido")
+    print("%d é um número primo" % numero)
 
-        continue
+else:
 
-    if numero == 2 or numero == 3:
+    for contador in range(2, numero + 1):
+
+        if numero % contador != 0:
+
+            confirmacao += 1
+
+    if confirmacao == numero:
 
         print("%d é um número primo" % numero)
 
     else:
 
-        for contador2 in range(2, numero + 1):
-
-            if numero % contador2 != 0:
-
-                confirmacao += 1
-
-        if confirmacao == numero:
-
-            print("%d é um número primo" % numero)
-
-        else:
-
-           print("%d não é um número primo" % numero)
-
-    print()
-
-    contador = int(input("Digite 0 para interromper a execução: "))
-
-    print()
+       print("%d não é um número primo" % numero)
