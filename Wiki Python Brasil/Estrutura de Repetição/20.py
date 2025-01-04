c5109a3177ff05e780181c5ab3852069e79ed0e2
marvin <marvinsilc@protonmail.ch>
2025-01-04 10:11:18 -0300

    Resolving new challenges

--- Wiki Python Brasil/Estrutura de Repetição/20.py
+++ Wiki Python Brasil/Estrutura de Repetição/20.py
@@ -1,1 +1,21 @@
+contador = 1
 
+while contador != 0:
+
+    numero = int(input("Digite um número para saber a fatorial: "))
+
+    if numero > 16:
+
+        print("Somente número menores que 16")
+
+        continue
+
+    resultado_fatorial = numero
+
+    for contador in range(numero, 1, -1):
+
+        resultado_fatorial *= contador - 1
+
+    print("Fatorial de %d é %d" % (numero, resultado_fatorial))
+
+    contador = int(input("Digite 0 para interromper a execução: "))
