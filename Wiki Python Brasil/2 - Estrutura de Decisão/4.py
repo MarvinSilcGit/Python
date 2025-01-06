709dc3d4a074be04665045e10e31197764794f86
marvin <marvinsilc@protonmail.ch>
2025-01-06 20:36:49 -0300

    Resolving new challenges

--- Wiki Python Brasil/2 - Estrutura de Decisão/4.py
+++ Wiki Python Brasil/2 - Estrutura de Decisão/4.py
@@ -1,18 +1,18 @@
-listaConsoante = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ç",
+lista_consoantes = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ç",
                   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ç"]
 
-listaVogal = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+lista_vogais = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
 
-letraBusca = input("Digite a letra para saber se é vogal ou consoante: ")
+letra_busca = input("Digite a letra para saber se é vogal ou consoante: ")
 
-if letraBusca in listaVogal:
+if letra_busca in lista_vogais:
 
-    print("A letra '%s' é Vogal" % letraBusca)
+    print("A letra '%s' é Vogal" % letra_busca)
 
-elif letraBusca in listaConsoante:
+elif letra_busca in lista_consoantes:
 
-    print("A letra '%s' é Consoante" % letraBusca)
+    print("A letra '%s' é Consoante" % letra_busca)
 
 else:
 
-    print("A letra '%s' não é nem vogal nem consoante" % letraBusca)
+    print("A letra '%s' não é nem vogal nem consoante" % letra_busca)
