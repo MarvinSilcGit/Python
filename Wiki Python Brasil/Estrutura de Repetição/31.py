d87b01d208802f37c2f6b5fdaa6632d9dc1a918d
marvin <marvinsilc@protonmail.ch>
2025-01-06 09:50:23 -0300

    Resolving new challenges

---
+++ Wiki Python Brasil/Estrutura de Repetição/31.py
@@ -0,0 +1,47 @@
+contador = 1
+
+contador2 = 1
+
+precoFinal = 0
+
+contador3 = 1
+
+troco = 0
+
+valorPagamento = 0
+
+while contador != 0:
+
+    while contador2 != 0:
+
+        print("Digite o preço da mercadoria %d ou 0 para finalizar a compra: " % contador3)
+
+        preco = float(input(""))
+
+        precoFinal += preco
+
+        contador3 += 1
+
+        if preco == 0:
+
+            contador2 = 0
+
+            contador3 = 1
+
+            print("Total: %.2f" % precoFinal)
+
+            valorPagamento = float(input("Digite o valor de pagamento: "))
+
+            troco = valorPagamento - precoFinal
+
+            if valorPagamento < precoFinal:
+
+                print("Valor insuficiente para o pagamento")
+
+                continue
+
+            else:
+
+                print("Troco: %.2f" % troco)
+
+                break
