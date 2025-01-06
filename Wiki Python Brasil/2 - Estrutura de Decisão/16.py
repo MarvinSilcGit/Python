254338d32eb965569732cda6af7988e46f504050
marvin <marvinsilc@protonmail.ch>
2025-01-06 17:22:48 -0300

    Resolving new challenges

--- Wiki Python Brasil/2 - Estrutura de Decisão/16.py
+++ Wiki Python Brasil/2 - Estrutura de Decisão/16.py
@@ -14,10 +14,22 @@ else:
 
     delta = b ** 2 - 4 * a * c
 
-    print(math.sqrt(delta))
-
     raiz_negativa = (- b - math.sqrt(delta)) / 2 * a
 
     raiz_positiva = (- b + math.sqrt(delta)) / 2 * a
 
-    print(raiz_negativa, raiz_positiva)
+    if delta < 0:
+
+        print("A equação não possui raizes reais")
+
+    elif delta == 0:
+
+        print("A equação possui apenas uma raiz real")
+
+        print("A raiz positiva é %d" % raiz_positiva)
+
+    else:
+
+        print("A equação possui duas raiz reais")
+
+        print("A raiz negativa é %d e a raiz positiva é %d" % (raiz_negativa, raiz_positiva))
