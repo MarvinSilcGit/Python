64828431a8127d7a69dd94b1a20e085b6576e66e
marvin <marvinsilc@protonmail.ch>
2025-01-12 15:35:37 -0300

    Fixing minor erros

--- Functions/Matemáticas.py
+++ Functions/Matemáticas.py
@@ -424,32 +424,28 @@ def gerador_tabuada_simples (numero_tabuada: int):
     return 0
 
 
-def gerador_tabuada_inicio_fim (numero_inicial_tabuada: int, numero_final_tabuada: int):
+def gerador_tabuada_inicio_fim (numero_inicial: int, numero_final: int):
 
-    inicio_tabuada = numero_inicial_tabuada
+    inicio_tabuada = numero_inicial
 
-    fim_tabudada = numero_final_tabuada
-
-    for contador in range(1, fim_tabudada + 1):
+    for contador in range(1, numero_final + 1):
 
         print("%d x %d = %d " % (inicio_tabuada, contador, inicio_tabuada * contador))
 
     return 0
 
 
-def gerador_tabuada_inicio_fim_razao (numero_inicial_tabuada: int, numero_final_tabuada: int, razao: int):
+def gerador_tabuada_inicio_fim_razao (numero_inicial: int, numero_final: int, razao: int):
 
-    inicio_tabuada = numero_inicial_tabuada
+    inicio_tabuada = numero_inicial
 
-    fim_tabudada = numero_final_tabuada
-
-    razao = razao
-
-    for contador in range(1, fim_tabudada + 1, razao):
+    for contador in range(1, numero_final + 1, razao):
 
         print("%d x %d = %d " % (inicio_tabuada, contador, inicio_tabuada * contador))
 
     return 0
+
+print(gerador_tabuada_inicio_fim_razao(2, 20, 2))
 
 
 def numero_fizz_buzz (numero_limite: int):
