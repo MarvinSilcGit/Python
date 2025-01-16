bb8411d7b2a9133b675cbf8b32de50c2d71e3aae
marvin <marvinsilc@protonmail.ch>
2025-01-16 20:15:52 -0300

    Removing redundancies

--- Functions/Matemáticas.py
+++ Functions/Matemáticas.py
@@ -452,3 +452,17 @@ def juros_compostos (valor_inicial: float, aporte_mensal: float, juros_anual: fl
         ganho_bruto_total += aporte_mensal
 
     return ""
+
+
+def radiano_grau (radiano: float):
+
+    import math
+
+    return  "%.2f radianos equivalem à %.2f°" % (radiano, radiano * 180 / math.pi)
+
+
+def grau_radiano (grau: float):
+
+    import math
+
+    return "%.2f° equivalem à %.3f radianos" % (grau, grau * math.pi / 180)
