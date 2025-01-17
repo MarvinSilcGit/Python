7b232daf9e3a629b5012fd4eef268918020a7799
marvin <marvinsilc@protonmail.ch>
2025-01-17 10:00:40 -0300

    Resolving new challenges and

--- Functions/Matemáticas/Angles.py
+++ Functions/Matemáticas/Angles.py
@@ -1,12 +1,48 @@
-def radiano_grau (radiano: float):
+def radiano_graus (radiano: float):
 
     import math
 
-    return  "%.2f radianos equivalem à %.2f°" % (radiano, radiano * 180 / math.pi)
+    graus = radiano * 180 / math.pi
+
+    return  "%.2f radianos equivalem à %.2f° Graus" % (radiano, graus)
 
 
-def grau_radiano (grau: float):
+def radiano_grados (radiano: float):
 
     import math
 
-    return "%.2f° equivalem à %.3f radianos" % (grau, grau * math.pi / 180)
+    grados = radiano * 200 / math.pi
+
+    return  "%.2f radianos equivalem à %.2f° Grados" % (radiano, grados)
+
+
+def graus_radiano (grau: float):
+
+    import math
+
+    radiano = grau * math.pi / 180
+
+    return "%.2f° Graus equivalem à %.3f Radianos" % (grau, radiano)
+
+
+def graus_grados (graus: float):
+
+    grados = graus * 200 / 180
+
+    return "%.2f° Graus equivalem à %.2f° Grados" % (graus, grados)
+
+
+def grados_radiano (grado: float):
+
+    import math
+
+    radiano = grado * math.pi / 200
+
+    return "%.2f° Grados equivalem à %.2f° Radianos" % (grado, radiano)
+
+
+def grados_graus (grado: float):
+
+    graus = grado / 100 * 90
+
+    return "%.2f° Grados equivalem à %.2f° Graus" % (grado, graus)
