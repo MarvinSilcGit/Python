05b3dc34fea8024252f4a50ea8d3a1c6e55a4fe6
marvin <marvinsilc@protonmail.ch>
2025-01-19 10:40:52 -0300

    Adding new functions

---
+++ Functions/Matemáticas/Speed.py
@@ -0,0 +1,23 @@
+## Miles to many
+
+def milhas_quilometros (milhas: float):
+
+    return f"{milhas:,.2f} Milhas/h equivalem à {milhas * 1.609:,.2f} /h"
+
+
+def milhas_pes (milhas: float):
+
+    return f"{milhas:,.2f} Milhas/h equivalem à {milhas * 1.467:,.2f} Pés/s"
+
+
+def milhas_metros (milhas: float):
+
+    return f"{milhas:,.2f} Milhas/h equivalem à {milhas / 2.237:,.2f} Metros/s"
+
+
+def milhas_nos (milhas: float):
+
+    return f"{milhas:,.2f} Milhas/h equivalem à {milhas / 1.151:,.2f} Nós"
+
+
+# Meters to many
