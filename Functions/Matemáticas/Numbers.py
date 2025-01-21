b085a3e10f68c040c8e24ff9719cce5335fdad64
marvin <marvinsilc@protonmail.ch>
2025-01-21 14:38:18 -0300

    Adding new functions and reorganizing the files

---
+++ Functions/Matemáticas/Numbers.py
@@ -0,0 +1,268 @@
+def resto_divisao (valor1: float, valor2: float):
+
+    resto = 0
+
+    if valor1 == 0 or valor2 == 0:
+
+        return "Divisão por zero inválida"
+
+    else:
+
+        if valor1 == valor2:
+
+            resto = 0
+
+        elif valor1 > valor2:
+
+            while valor2 + resto <= valor1:
+
+                resto += valor2
+
+            resto = valor1 - resto
+
+        else:
+
+            while valor1 + resto < valor2:
+
+                resto += valor1
+
+            while resto != valor2:
+
+                resto += 1
+
+        return "O resto da divisão entre %.1f e %.1f é: %.1f" % (valor1, valor2, resto)
+
+
+def resto_inteiro_divisao (valor1: float, valor2: float):
+
+    resto_inteiro = 0
+
+    resto = 0
+
+    if valor1 == 0 or valor2 == 0:
+
+        return "Divisão por zero inválida"
+
+    else:
+
+        if valor1 == valor2:
+
+            resto_inteiro = 1
+
+        elif valor1 > valor2:
+
+            while valor2 + resto <= valor1:
+
+                resto_inteiro += 1
+
+                resto += valor2
+
+        else:
+
+            while valor1 + resto < valor2:
+
+                resto += valor1
+
+            while resto != valor2:
+
+                resto += 1
+
+        return "O resto inteiro da divisão entre %.1f e %.1f é: %d" % (valor1, valor2, resto_inteiro)
+
+
+def raiz_quadrada (numero: float):
+
+    base = 2
+
+    resultado_raiz_quadrada = (base + numero / base) / 2
+
+    resultado_raiz_quadrada = resultado_raiz_quadrada ** 2
+
+    while resultado_raiz_quadrada * resultado_raiz_quadrada - numero > 0.001:
+
+        base = resultado_raiz_quadrada
+
+        resultado_raiz_quadrada = (base + numero / base) / 2
+
+    return "A raiz aproximada de %d é: %.4f" % (numero, resultado_raiz_quadrada)
+
+
+def numero_primo (numero: int):
+
+    confirmacao = 2
+
+    if numero == 0 or numero == 1:
+
+        return "Esse número é inválido"
+
+    if numero ==  2 or numero == 3:
+
+        return "%d é um número primo" % numero
+
+    else:
+
+        for contador in range(confirmacao, numero + 1):
+
+            if numero % contador != 0:
+
+                confirmacao += 1
+
+        if confirmacao == numero:
+
+            return "%d é um número primo, pois é divisível somente por ele e por 1" % numero
+
+        else:
+
+            print("O número %d não é primo, pois ele é divisível por: " % numero, end="")
+
+            for contador in range(1, numero + 1):
+
+                if numero % contador == 0:
+
+                    if numero - contador == 0:
+
+                        print(contador, end=" ")
+
+                    else:
+
+                        print(contador, end=", ")
+            return ""
+
+
+def numero_par_impar (numero: int):
+
+    if numero % 2 == 0:
+
+        return "O número %d é par" % numero
+
+    else:
+
+        return "O número %d é impar" % numero
+
+
+def calculo_fatorial (numero: int):
+
+    resultado_fatorial = numero
+
+    for contador in range(numero, 1, -1):
+
+        resultado_fatorial *= contador - 1
+
+    return "Fatorial de %d é %d" % (numero, resultado_fatorial)
+
+
+def ano_bissexto (ano: int):
+
+    if ano % 4 == 0:
+
+        if ano % 100 != 0:
+
+            return "O ano %d é bissexto" % ano
+
+        elif ano % 100 == 0:
+
+            return "O ano %d é bissexto especial" % ano
+
+    else:
+
+        return "O ano %d não é bissexto" % ano
+
+
+def numero_decimal_inteiro (numero: float):
+
+    import math
+
+    numero_decimal = numero
+
+    numero_inteiro = math.trunc(numero_decimal)
+
+    if numero_decimal > numero_inteiro:
+
+        return "O número %.1f é decimal" % numero_decimal
+
+    else:
+
+        numero_decimal = str(numero_decimal)
+
+        if "." in numero_decimal:
+
+            return "O número %s é decimal" % numero_decimal
+
+        else:
+
+            return "O número %d não é decimal" % numero_inteiro
+
+
+def numero_fizz_buzz (numero_limite: int):
+
+    if numero_limite <= 0:
+
+        return "Número menores que 1 não são válidos"
+
+    if numero_limite % 3 == 0 and numero_limite % 5 == 0 and numero_limite != 0:
+
+        return "O número %d é FizzBuzz" % numero_limite
+
+    elif numero_limite % 3 == 0:
+
+        return "O número %d é Fizz" % numero_limite
+
+    elif numero_limite % 5 == 0:
+
+        return "O número %d é Buzz" % numero_limite
+
+    else:
+
+        return "O número %d não é nem Fizz nem Buzz" % numero_limite
+
+
+def binary_decimal (numero: str):
+
+    tamanho_decimal = len(numero)
+
+    decimal = 0
+
+    expoente = tamanho_decimal - 1
+
+    for contador in numero:
+
+        if contador == "1":
+
+            decimal += (1 * 2 ** expoente)
+
+        elif contador == "0":
+
+            decimal += (0 * 2 ** expoente)
+
+        else:
+
+            return 'Esse não é um número binário'
+
+        expoente -= 1
+
+        numero = int(numero)
+
+    return "%d binário representa %d em decimal" % (numero, decimal)
+
+
+def decimal_binary (numero: int):
+
+    resto = []
+
+    binario = ""
+
+    numero_decimal = numero
+
+    while numero_decimal != 0:
+
+        resto.insert(0, numero_decimal % 2)
+
+        numero_decimal = numero_decimal // 2
+
+    for contador in resto:
+
+        binario += str(contador)
+
+    binario = int(binario)
+
+    return "%d decimal representa %s em binário" % (numero, binario)
--- Functions/Matemáticas/Uso Geral.py
+++ Functions/Matemáticas/Uso Geral.py
@@ -1,148 +1,6 @@
 #How to import: from Functions.Matemáticas import resto_divisao or import *
 
 
-def resto_divisao (valor1: float, valor2: float):
-
-    resto = 0
-
-    if valor1 == 0 or valor2 == 0:
-
-        return "Divisão por zero inválida"
-
-    else:
-
-        if valor1 == valor2:
-
-            resto = 0
-
-        elif valor1 > valor2:
-
-            while valor2 + resto <= valor1:
-
-                resto += valor2
-
-            resto = valor1 - resto
-
-        else:
-
-            while valor1 + resto < valor2:
-
-                resto += valor1
-
-            while resto != valor2:
-
-                resto += 1
-
-        return "O resto da divisão entre %.1f e %.1f é: %.1f" % (valor1, valor2, resto)
-
-
-def resto_inteiro_divisao (valor1: float, valor2: float):
-
-    resto_inteiro = 0
-
-    resto = 0
-
-    if valor1 == 0 or valor2 == 0:
-
-        return "Divisão por zero inválida"
-
-    else:
-
-        if valor1 == valor2:
-
-            resto_inteiro = 1
-
-        elif valor1 > valor2:
-
-            while valor2 + resto <= valor1:
-
-                resto_inteiro += 1
-
-                resto += valor2
-
-        else:
-
-            while valor1 + resto < valor2:
-
-                resto += valor1
-
-            while resto != valor2:
-
-                resto += 1
-
-        return "O resto inteiro da divisão entre %.1f e %.1f é: %d" % (valor1, valor2, resto_inteiro)
-
-
-def raiz_quadrada (numero: float):
-
-    base = 2
-
-    resultado_raiz_quadrada = (base + numero / base) / 2
-
-    resultado_raiz_quadrada = resultado_raiz_quadrada ** 2
-
-    while resultado_raiz_quadrada * resultado_raiz_quadrada - numero > 0.001:
-
-        base = resultado_raiz_quadrada
-
-        resultado_raiz_quadrada = (base + numero / base) / 2
-
-    return "A raiz aproximada de %d é: %.4f" % (numero, resultado_raiz_quadrada)
-
-
-def numero_primo (numero: int):
-
-    confirmacao = 2
-
-    if numero == 0 or numero == 1:
-
-        return "Esse número é inválido"
-
-    if numero ==  2 or numero == 3:
-
-        return "%d é um número primo" % numero
-
-    else:
-
-        for contador in range(confirmacao, numero + 1):
-
-            if numero % contador != 0:
-
-                confirmacao += 1
-
-        if confirmacao == numero:
-
-            return "%d é um número primo, pois é divisível somente por ele e por 1" % numero
-
-        else:
-
-            print("O número %d não é primo, pois ele é divisível por: " % numero, end="")
-
-            for contador in range(1, numero + 1):
-
-                if numero % contador == 0:
-
-                    if numero - contador == 0:
-
-                        print(contador, end=" ")
-
-                    else:
-
-                        print(contador, end=", ")
-            return ""
-
-
-def numero_par_impar (numero: int):
-
-    if numero % 2 == 0:
-
-        return "O número %d é par" % numero
-
-    else:
-
-        return "O número %d é impar" % numero
-
-
 def palindromo (valor):
 
     contador2 = 0
@@ -258,17 +116,6 @@ def atm_machine (valor_pagamento: float):
 print(atm_machine(11.5))"""
 
 
-def calculo_fatorial (numero: int):
-
-    resultado_fatorial = numero
-
-    for contador in range(numero, 1, -1):
-
-        resultado_fatorial *= contador - 1
-
-    return "Fatorial de %d é %d" % (numero, resultado_fatorial)
-
-
 def raio_circulo (area: float):
 
     return 3.14 * (area ** 2)
@@ -329,48 +176,6 @@ def triangulo_validade_tipo (lado1: int, lado2: int, lado3: int):
                 return "Esse é um triângulo isóceles"
 
 
-def ano_bissexto (ano: int):
-
-    if ano % 4 == 0:
-
-        if ano % 100 != 0:
-
-            return "O ano %d é bissexto" % ano
-
-        elif ano % 100 == 0:
-
-            return "O ano %d é bissexto especial" % ano
-
-    else:
-
-        return "O ano %d não é bissexto" % ano
-
-
-def numero_decimal_inteiro (numero: float):
-
-    import math
-
-    numero_decimal = numero
-
-    numero_inteiro = math.trunc(numero_decimal)
-
-    if numero_decimal > numero_inteiro:
-
-        return "O número %.1f é decimal" % numero_decimal
-
-    else:
-
-        numero_decimal = str(numero_decimal)
-
-        if "." in numero_decimal:
-
-            return "O número %s é decimal" % numero_decimal
-
-        else:
-
-            return "O número %d não é decimal" % numero_inteiro
-
-
 def gerador_tabuada_simples (numero_tabuada: int):
 
     for contador in range(1, 10 + 1):
@@ -398,29 +203,6 @@ def gerador_tabuada_inicio_fim_razao (numero_inicial: int, numero_final: int, ra
     return 0
 
 
-def numero_fizz_buzz (numero_limite: int):
-
-    if numero_limite <= 0:
-
-        return "Número menores que 1 não são válidos"
-
-    if numero_limite % 3 == 0 and numero_limite % 5 == 0 and numero_limite != 0:
-
-        return "O número %d é FizzBuzz" % numero_limite
-
-    elif numero_limite % 3 == 0:
-
-        return "O número %d é Fizz" % numero_limite
-
-    elif numero_limite % 5 == 0:
-
-        return "O número %d é Buzz" % numero_limite
-
-    else:
-
-        return "O número %d não é nem Fizz nem Buzz" % numero_limite
-
-
 def juros_compostos (valor_inicial: float, aporte_mensal: float, juros_anual: float, quantidade_meses: int):
 
     ganho_bruto_total = valor_inicial
@@ -440,56 +222,71 @@ def juros_compostos (valor_inicial: float, aporte_mensal: float, juros_anual: fl
     return ""
 
 
-def binary_decimal (numero: str):
+def validade_cpf (valor):
 
-    tamanho_decimal = len(numero)
 
-    decimal = 0
+    if len(valor) != 11:
 
-    expoente = tamanho_decimal - 1
+        return "Cpf inválido"
 
-    for contador in numero:
+    else:
 
-        if contador == "1":
+        posicao_j = 0
 
-            decimal += (1 * 2 ** expoente)
+        posicao_k = 0
 
-        elif contador == "0":
+        cpf = str(valor)
 
-            decimal += (0 * 2 ** expoente)
+        estado = ""
+
+        if cpf[8] == "0":
+
+            estado = "Rio Grande do sul"
+
+        elif cpf[8] == "1":
+
+            estado = "Distrido Federal, Goiás, Mato Grosso do Sul ou Tocantins"
+
+        elif cpf[8] == "2":
+
+            estado = "Pará, Amazonas, Acre, Amapá, Rondônio ou Roraíma"
+
+        elif cpf[8] == "3":
+
+            estado = "Ceará, Maranhão ou Piauí"
+
+        elif cpf[8] == "4":
+
+            estado = "Pernambuco, Rio Grande do Norte, Paraíba ou Alagoas"
+
+        elif cpf[8] == "5":
+
+            estado = "Bahia ou Sergipe"
+
+        elif cpf[8] == "6":
+
+            estado = "Minas Gerais"
+
+        elif cpf[8] == "7":
+
+            estado = "Rio de Janeiro ou Espírito Santo"
+
+        elif cpf[8] == "8":
+
+            estado = "São Paulo"
+
+        elif cpf[8] == "9":
+
+            estado = "Paraná ou Santa Catarina"
 
         else:
 
-            return 'Esse não é um número binário'
+            return "cpf inválido"
 
-        expoente -= 1
+        for contador in cpf:
 
-        numero = int(numero)
+            print(contador)
 
-    return "%d binário representa %d em decimal" % (numero, decimal)
+        return estado
 
-print(binary_decimal('10000'))
-
-def decimal_binary (numero: int):
-
-    resto = []
-
-    binario = ""
-
-    numero_decimal = numero
-
-    while numero_decimal != 0:
-
-        resto.insert(0, numero_decimal % 2)
-
-        numero_decimal = numero_decimal // 2
-
-    for contador in resto:
-
-        binario += str(contador)
-
-    binario = int(binario)
-
-    return "%d decimal representa %s em binário" % (numero, binario)
-
-print(decimal_binary(16))
+print(validade_cpf('12348095523'))
