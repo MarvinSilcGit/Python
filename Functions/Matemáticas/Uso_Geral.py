2ed294a0a8c5bd826248c13ba1c86601d32398d3
marvin <marvinsilc@protonmail.ch>
2025-01-28 15:16:36 -0300

    Fixing minor errors and redundancies

--- Functions/Matemáticas/Uso_Geral.py
+++ Functions/Matemáticas/Uso_Geral.py
@@ -246,90 +246,70 @@ def formatar_cpf (cpf: str):
 
 def validade_cpf (cpf):
 
-    if len(cpf) != 11:
+    cpf_validador = list(cpf[0:9])
 
-        return "CPF inválido"
+    cpf_validador = ''.join(cpf_validador)
+
+    dicionario_estados = {"1": "Distrito Federal, Goiás, Mato Grosso do Sul ou Tocantins", "2": "Pará, Amazonas, Acre, Amapá, Rondônia ou Roraíma",
+                          "3": "Ceará, Maranhão ou Piauí",
+                          "4": "Pernambuco, Rio Grande do Norte, Paraíba ou Alagoas", "5": "Bahia ou Sergipe", "6": "Minas Gerais",
+                          "7": "Rio de Janeiro ou Espírito Santo", "8": "São Paulo",
+                          "9":"Paraná ou Santa Catarina"}
+
+    resultado_posicao_j = 0
+
+    resultado_posicao_k = 0
+
+    contador = 10
+
+    for contador2 in cpf_validador:
+
+        resultado_posicao_j += int(contador2) * contador
+
+        contador -= 1
+
+    if resultado_posicao_j % 11 < 2:
+
+        cpf_validador += '0'
+
+    elif 2 <= resultado_posicao_j % 11 <= 10: # equals to: elif resultado_posicao_j >= 2 and resultado_posicao_j <= 10:
+
+        cpf_validador += str(11 - (resultado_posicao_j % 11))
+
+    contador = 11
+
+    for contador2 in cpf_validador:
+
+        resultado_posicao_k += int(contador2) * contador
+
+        contador -= 1
+
+    if resultado_posicao_k % 11 < 2:
+
+        cpf_validador += '0'
+
+    elif 2 <= resultado_posicao_k % 11 <= 10: # equals to: elif resultado_posicao_k >= 2 and resultado_posicao_k <= 10:
+
+        cpf_validador += str(11 - (resultado_posicao_k % 11))
+
+    if cpf == cpf_validador:
+
+        return f"O CPF {formatar_cpf(cpf)} é válido, sendo emitido em {dicionario_estados[cpf[8]]}"
 
     else:
 
-        dicionario_estados = {"1": "Distrito Federal, Goiás, Mato Grosso do Sul ou Tocantins", "2": "Pará, Amazonas, Acre, Amapá, Rondônia ou Roraíma",
-                              "3": "Ceará, Maranhão ou Piauí",
-                              "4": "Pernambuco, Rio Grande do Norte, Paraíba ou Alagoas", "5": "Bahia ou Sergipe", "6": "Minas Gerais",
-                              "7": "Rio de Janeiro ou Espírito Santo", "8": "São Paulo",
-                              "9":"Paraná ou Santa Catarina"}
-
-        resultado_posicao_j = 0
-
-        resultado_posicao_k = 0
-
-        contador = 10
-
-        estado = cpf[8]
-
-        for contador2 in range(9):
-
-            resultado_posicao_j += int(cpf[contador2]) * contador
-
-            contador -= 1
-
-        if resultado_posicao_j % 11 < 2:
-
-            posicao_j = 0
-
-        elif 2 <= resultado_posicao_j % 11 <= 10: # equals to: elif resultado_posicao_j >= 2 and resultado_posicao_j <= 10
-
-            posicao_j = 11 - (resultado_posicao_j % 11)
-
-        else:
-
-            return "CPF inválido"
-
-        contador = 11
-
-        for contador2 in range(10):
-
-            resultado_posicao_k += int(cpf[contador2]) * contador
-
-            contador -= 1
-
-        if resultado_posicao_k % 11 < 2:
-
-            posicao_k = 0
-
-        elif 2 <= resultado_posicao_k % 11 <= 10: # equals to: elif resultado_posicao_k >= 2 and resultado_posicao_k <= 10
-
-            posicao_k = 11 - (resultado_posicao_k % 11)
-
-        else:
-
-            return "CPF inválido"
-
-        posicao_k = str(posicao_k)
-
-        posicao_j = str(posicao_j)
-
-        if posicao_j == cpf[9] and posicao_k == cpf[10]:
-
-            cpf = formatar_cpf(cpf)
-
-            return f"O CPF {cpf} é válido, sendo emitido em {dicionario_estados[estado]}"
-
-        else:
-
-            return "CPF inválido"
+        return "CPF inválido"
 
 
 def gerador_cpf ():
 
     import random as aleatoriedade
 
-    cpf = []
+    cpf = ''
 
     for contador in range(11):
 
-        cpf.append(str(aleatoriedade.randint(0,9)))
-
-    cpf = ''.join(cpf)
+        cpf += str(aleatoriedade.randint(0,9))
 
     cpf = formatar_cpf(cpf)
 
@@ -417,7 +397,7 @@ def validade_cnpj(cnpj: str):
 
         return f"O CNPJ {cnpj} é inválido"
 
-print(validade_cnpj('59120772000100'))
+#print(validade_cnpj('59120772000100'))
 
 def gerador_cnpj ():
 
