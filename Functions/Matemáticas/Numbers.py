1fee1e5cb108de385e6784399eaa331a3eccf9c2
marvin <marvinsilc@protonmail.ch>
2025-01-29 17:48:11 -0300

    Fixing minor errors

--- Functions/Matemáticas/Numbers.py
+++ Functions/Matemáticas/Numbers.py
@@ -280,34 +280,38 @@ def equacao_segundo_grau_bhaskara (coeficiente_a: float, coeficiente_b: float, c
 
         delta = coeficiente_b ** 2 - 4 * coeficiente_a * coeficiente_c
 
-        x_1 = -coeficiente_b + float(raiz_quadrada(delta))
-
-        if numero_decimal_inteiro(x_1 / (2 * coeficiente_a)):
-
-            x_1 = Fraction(int(x_1), int(2 * coeficiente_a))
-
-        else:
-
-            x_1 = x_1 / (2 * coeficiente_a)
-
-        x_2 = -coeficiente_b - float(raiz_quadrada(delta))
-
-        if numero_decimal_inteiro(x_2 / (2 * coeficiente_a)):
-
-            x_2 = Fraction(int(x_2), int(2 * coeficiente_a))
-
-        else:
-
-            x_2 = x_2 / (2 * coeficiente_a)
-
         if delta < 0:
 
-            return "A equação não possui raizes reais"
-
-        elif delta == 0:
-
-            return f"A equação possui apenas uma raiz real. A raiz positiva é {x_1}"
+            return "A equação não possui raízes reais"
 
         else:
 
-            return f"Equação completa. A raiz positiva é {x_1}. A raiz negativa é {x_2}"
+            x_1 = -coeficiente_b + float(raiz_quadrada(delta))
+
+            if numero_decimal_inteiro(x_1 / (2 * coeficiente_a)):
+
+                x_1 = Fraction(int(x_1), int(2 * coeficiente_a))
+
+            else:
+
+                x_1 /= (2 * coeficiente_a)
+
+            x_2 = -coeficiente_b - float(raiz_quadrada(delta))
+
+            if numero_decimal_inteiro(x_2 / (2 * coeficiente_a)):
+
+                x_2 = Fraction(int(x_2), int(2 * coeficiente_a))
+
+            else:
+
+                x_2 = x_2 / (2 * coeficiente_a)
+
+            if delta == 0:
+
+                return f"A equação possui apenas uma raiz real. A raiz positiva é {x_1}"
+
+            else:
+
+                return f"Equação completa. A raiz positiva é {x_1}. A raiz negativa é {x_2}"
+
+print(equacao_segundo_grau_bhaskara(1, -6, -8))
