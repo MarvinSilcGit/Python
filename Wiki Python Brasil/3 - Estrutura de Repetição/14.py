1283bad9cf16453c8d15cdf5a4ef9faf9c4cbf7c
marvin <marvinsilc@protonmail.ch>
2025-06-20 20:29:10 -0300

    Adding ternary operator

--- Wiki Python Brasil/3 - Estrutura de Repetição/14.py
+++ Wiki Python Brasil/3 - Estrutura de Repetição/14.py
@@ -2,10 +2,4 @@ for contador in range(10):
 
     numero = int(input("Digite o número para saber se é par ou ímpar: "))
 
-    if numero % 2 != 0:
-
-        print(f"O número {numero} é par")
-
-    else:
-
-        print(f"O número {numero} é ímpar")
+    print(f"O número {numero} é ímpar") if numero % 2 != 0 else print(f"O número {numero} é par")
