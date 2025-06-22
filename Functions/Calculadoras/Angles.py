da0508043ce8f80b5e6ea07f54bb65d524905c16
marvin <marvinsilc@protonmail.ch>
2025-06-22 17:16:01 -0300

    Simplifying the code

--- Functions/Calculadoras/Angles.py
+++ Functions/Calculadoras/Angles.py
@@ -4,39 +4,39 @@ def radiano_graus (radiano: float):
 
     graus = radiano * 180 / math.pi
 
-    return f"{radiano:,.2f}° Radianos equivalem à {graus:,.2f}° Graus"
+    return f"{graus:,.2f}"
 
 
 def radiano_grados (radiano: float):
 
     grados = radiano * 200 / math.pi
 
-    return f"{radiano:,.2f}° Radianos equivalem à {grados:,.2f}° Grados"
+    return f"{grados:,.2f}°"
 
 
 def graus_radiano (grau: float):
 
     radiano = grau * math.pi / 180
 
-    return f"{grau:,.2f}° Graus equivalem à {radiano:,.2f} Radianos"
+    return f"{radiano:,.2f}"
 
 
 def graus_grados (graus: float):
 
     grados = graus * 200 / 180
 
-    return f"{graus:,.2f}° Graus equivalem à {grados:,.2f}° Grados"
+    return f"{grados:,.2f}"
 
 
 def grados_radiano (grado: float):
 
     radiano = grado * math.pi / 200
 
-    return f"{grado:,.2f}° Grados equivalem à {radiano:,.2f}° Radianos"
+    return f"{radiano:,.2f}"
 
 
 def grados_graus (grado: float):
 
     graus = grado / 100 * 90
 
-    return f"{grado:,.2f}° Grados equivalem à {graus:,.2f}° Graus"
+    return f"{graus:,.2f}"
