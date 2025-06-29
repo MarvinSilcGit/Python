9a9e8e09f6255fe5d18cd55860e93e383a543b18
marvin <marvinsilc@protonmail.ch>
2025-06-28 21:41:16 -0300

    Updating the alphabet

---
+++ My own stuff/Alfabeto.py
@@ -0,0 +1,5 @@
+def listagem_alfabeto (letra: str):
+
+    lista_alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ç']
+
+    return letra.upper() in lista_alfabeto
